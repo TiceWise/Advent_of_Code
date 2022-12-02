af7683af9aeca0b550380fc5739f82ff312d6444
TiceWise <thijsdgroot@gmail.com>
2022-12-02 09:10:22 +0100

    AoC 2022: Day 2 - ready for Day 3

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ export function AoC2022DayX(input: string): Answer {
 getAnswer()
 
 async function getAnswer() {
-  const day = 2
+  const day = 3
   const year = 2022
   const input = await getAoCInputData(day, year)
   const answer = AoC2022Day2(input)
@@ -28,11 +28,136 @@ interface Answer {
   answerQuestion2: number
 }
 
-export function AoC2022Day2(input: string): Answer {
+export function AoC2022Day3(input: string): Answer {
   let answerQuestion1 = 0
   let answerQuestion2 = 0
 
   return { answerQuestion1, answerQuestion2 }
+}
+
+export function AoC2022Day2(input: string): Answer {
+  const games = input.split('\n')
+
+  let totalScore = 0
+  let totalNewScore = 0
+
+  games.forEach((game) => {
+    const [opponent, me] = game.split(' ')
+
+    interface OutcomeMap {
+      [key: string]: string
+    }
+
+    // first column: opponent
+    // A rock
+    // B paper
+    // C scissors
+    // plus outcome: 0 lost, 3 draw, 6 won
+
+    // second column: what i must play
+    // X Rock, 1
+    // Y paper, 2
+    // Z scissors, 3
+
+    const handMap: OutcomeMap = {
+      A: 'rock',
+      B: 'paper',
+      C: 'scissors',
+      X: 'rock',
+      Y: 'paper',
+      Z: 'scissors',
+    }
+
+    if (handMap[me] == 'rock') {
+      totalScore += 1
+    }
+    if (handMap[me] == 'paper') {
+      totalScore += 2
+    }
+    if (handMap[me] == 'scissors') {
+      totalScore += 3
+    }
+
+    if (handMap[opponent] == 'rock') {
+      if (handMap[me] == 'rock') {
+        totalScore += 3
+      }
+      if (handMap[me] == 'paper') {
+        totalScore += 6
+      }
+    }
+    if (handMap[opponent] == 'paper') {
+      if (handMap[me] == 'paper') {
+        totalScore += 3
+      }
+      if (handMap[me] == 'scissors') {
+        totalScore += 6
+      }
+    }
+    if (handMap[opponent] == 'scissors') {
+      if (handMap[me] == 'rock') {
+        totalScore += 6
+      }
+      if (handMap[me] == 'scissors') {
+        totalScore += 3
+      }
+    }
+
+    // PART 2
+
+    const outcomeMap: OutcomeMap = {
+      X: 'lose',
+      Y: 'draw',
+      Z: 'win',
+    }
+
+    // 1 rock, 2 paper, 3 scissors
+
+    if (handMap[opponent] == 'rock') {
+      if (outcomeMap[me] == 'lose') {
+        // scisors
+        totalNewScore += 3
+      }
+      if (outcomeMap[me] == 'draw') {
+        // rock
+        totalNewScore += 1 + 3
+      }
+      if (outcomeMap[me] == 'win') {
+        // paper
+        totalNewScore += 2 + 6
+      }
+    }
+    if (handMap[opponent] == 'paper') {
+      if (outcomeMap[me] == 'lose') {
+        // rock
+        totalNewScore += 1
+      }
+      if (outcomeMap[me] == 'draw') {
+        // paper
+        totalNewScore += 2 + 3
+      }
+      if (outcomeMap[me] == 'win') {
+        // scis
+        totalNewScore += 3 + 6
+      }
+    }
+    if (handMap[opponent] == 'scissors') {
+      if (outcomeMap[me] == 'lose') {
+        // paper
+        totalNewScore += 2
+      }
+      if (outcomeMap[me] == 'draw') {
+        // sciss
+        totalNewScore += 3 + 3
+      }
+      if (outcomeMap[me] == 'win') {
+        // rock
+        totalNewScore += 1 + 6
+      }
+    }
+  })
+
+  return { answerQuestion1: totalScore, answerQuestion2: totalNewScore }
 }
 
 export function AoC2022Day1(input: string): Answer {
