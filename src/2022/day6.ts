40ac82a87304c7411ec32c0ae904cbe5bde5db69
TiceWise <thijsdgroot@gmail.com>
2022-12-06 12:16:51 +0100

    AoC 2022: Day 6 omg so much simpler how did i not see this, this morning

--- src/2022/day6.ts
+++ src/2022/day6.ts
@@ -1,36 +1,16 @@
 import { Answer } from '../models/models'
 
-function getUnique4Pos(dataIn: string): number {
-  for (let i = 3; i < dataIn.length; i++) {
-    const pos0 = dataIn[i - 3]
-    const pos1 = dataIn[i - 2]
-    const pos2 = dataIn[i - 1]
-    const pos3 = dataIn[i]
-
-    console.log(pos0)
-    if (pos0 !== pos1 && pos0 !== pos2 && pos0 !== pos3 && pos1 !== pos2 && pos1 !== pos3 && pos2 !== pos3) {
-      return i
-    }
-  }
-  return 0
+function isUnique(str: string) {
+  // we could add an extra check, that the length is also the messageLength
+  return new Set(str).size == str.length
 }
 
 function getUniqueXPos(dataIn: string, messageLength: number): number {
-  for (let i = messageLength - 1; i < dataIn.length; i++) {
-    const startMessagePos = i - messageLength + 1
-    const endMessagePos = i
+  for (let i = messageLength; i < dataIn.length; i++) {
+    const startMessagePos = i - messageLength
+    // const endMessagePos = i
 
-    let sameLetterFound = false
-
-    // we start with the longest message, also check each smaller message
-    for (let j = endMessagePos; j >= startMessagePos; j--) {
-      if (dataIn.slice(startMessagePos, j).includes(dataIn[j])) {
-        sameLetterFound = true
-        break
-      }
-    }
-
-    if (!sameLetterFound) {
+    if (isUnique(dataIn.slice(startMessagePos, i))) {
       return i
     }
   }
@@ -38,5 +18,5 @@ function getUniqueXPos(dataIn: string, messageLength: number): number {
 }
 
 export function AoC2022Day6(input: string): Answer {
-  return { answerQuestion1: getUniqueXPos(input, 4) + 1, answerQuestion2: getUniqueXPos(input, 14) + 1 }
+  return { answerQuestion1: getUniqueXPos(input, 4), answerQuestion2: getUniqueXPos(input, 14) }
 }
