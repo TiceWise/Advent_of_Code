e5896893390c1cda0c7e1980ee0024d2f89fc136
TiceWise <thijsdgroot@gmail.com>
2022-12-10 12:06:09 +0100

    AoC 2022: Day 10

--- src/2022/day10.spec.ts
+++ src/2022/day10.spec.ts
@@ -1,13 +1,164 @@
 import { AoC2022Day10 } from './day10'
 
-const input = ''
-
-describe.only('AoC 2022 day 10', () => {
-  it('should correctly answer question 1', () => {
+describe('AoC 2022 day 10', () => {
+  it.only('should correctly answer question 1.1', () => {
+    const input = 'noop\n' + 'addx 3\n' + 'addx -5\n'
     expect(AoC2022Day10(input).answerQuestion1).toBe(0)
   })
 
+  it.only('should correctly answer question 1.2', () => {
+    const input =
+      'addx 15\n' +
+      'addx -11\n' +
+      'addx 6\n' +
+      'addx -3\n' +
+      'addx 5\n' +
+      'addx -1\n' +
+      'addx -8\n' +
+      'addx 13\n' +
+      'addx 4\n' +
+      'noop\n' +
+      'addx -1\n' +
+      'addx 5\n' +
+      'addx -1\n' +
+      'addx 5\n' +
+      'addx -1\n' +
+      'addx 5\n' +
+      'addx -1\n' +
+      'addx 5\n' +
+      'addx -1\n' +
+      'addx -35\n' +
+      'addx 1\n' +
+      'addx 24\n' +
+      'addx -19\n' +
+      'addx 1\n' +
+      'addx 16\n' +
+      'addx -11\n' +
+      'noop\n' +
+      'noop\n' +
+      'addx 21\n' +
+      'addx -15\n' +
+      'noop\n' +
+      'noop\n' +
+      'addx -3\n' +
+      'addx 9\n' +
+      'addx 1\n' +
+      'addx -3\n' +
+      'addx 8\n' +
+      'addx 1\n' +
+      'addx 5\n' +
+      'noop\n' +
+      'noop\n' +
+      'noop\n' +
+      'noop\n' +
+      'noop\n' +
+      'addx -36\n' +
+      'noop\n' +
+      'addx 1\n' +
+      'addx 7\n' +
+      'noop\n' +
+      'noop\n' +
+      'noop\n' +
+      'addx 2\n' +
+      'addx 6\n' +
+      'noop\n' +
+      'noop\n' +
+      'noop\n' +
+      'noop\n' +
+      'noop\n' +
+      'addx 1\n' +
+      'noop\n' +
+      'noop\n' +
+      'addx 7\n' +
+      'addx 1\n' +
+      'noop\n' +
+      'addx -13\n' +
+      'addx 13\n' +
+      'addx 7\n' +
+      'noop\n' +
+      'addx 1\n' +
+      'addx -33\n' +
+      'noop\n' +
+      'noop\n' +
+      'noop\n' +
+      'addx 2\n' +
+      'noop\n' +
+      'noop\n' +
+      'noop\n' +
+      'addx 8\n' +
+      'noop\n' +
+      'addx -1\n' +
+      'addx 2\n' +
+      'addx 1\n' +
+      'noop\n' +
+      'addx 17\n' +
+      'addx -9\n' +
+      'addx 1\n' +
+      'addx 1\n' +
+      'addx -3\n' +
+      'addx 11\n' +
+      'noop\n' +
+      'noop\n' +
+      'addx 1\n' +
+      'noop\n' +
+      'addx 1\n' +
+      'noop\n' +
+      'noop\n' +
+      'addx -13\n' +
+      'addx -19\n' +
+      'addx 1\n' +
+      'addx 3\n' +
+      'addx 26\n' +
+      'addx -30\n' +
+      'addx 12\n' +
+      'addx -1\n' +
+      'addx 3\n' +
+      'addx 1\n' +
+      'noop\n' +
+      'noop\n' +
+      'noop\n' +
+      'addx -9\n' +
+      'addx 18\n' +
+      'addx 1\n' +
+      'addx 2\n' +
+      'noop\n' +
+      'noop\n' +
+      'addx 9\n' +
+      'noop\n' +
+      'noop\n' +
+      'noop\n' +
+      'addx -1\n' +
+      'addx 2\n' +
+      'addx -37\n' +
+      'addx 1\n' +
+      'addx 3\n' +
+      'noop\n' +
+      'addx 15\n' +
+      'addx -21\n' +
+      'addx 22\n' +
+      'addx -6\n' +
+      'addx 1\n' +
+      'noop\n' +
+      'addx 2\n' +
+      'addx 1\n' +
+      'noop\n' +
+      'addx -10\n' +
+      'noop\n' +
+      'noop\n' +
+      'addx 20\n' +
+      'addx 1\n' +
+      'addx 2\n' +
+      'addx 2\n' +
+      'addx -6\n' +
+      'addx -11\n' +
+      'noop\n' +
+      'noop\n' +
+      'noop\n'
+    expect(AoC2022Day10(input).answerQuestion1).toBe(13140)
+  })
+
   it('should correctly answer question 2', () => {
+    const input = ''
     expect(AoC2022Day10(input).answerQuestion2).toBe(0)
   })
 })
--- src/2022/day10.ts
+++ src/2022/day10.ts
@@ -3,5 +3,53 @@ import { Answer } from '../models/models'
 export function AoC2022Day10(input: string): Answer {
   const dataIn = input.split('\n')
 
-  return { answerQuestion1: 0, answerQuestion2: 0 }
+  let X = 1
+  let cycle = 0
+  let signalSum = 0
+
+  const displayWidth = 40
+  const displayHeight = 6
+
+  const display = [...Array(displayHeight)].map(() => Array(displayWidth).fill('.'))
+
+  function doCycleAndCalculateSignal() {
+    // start of cycle
+    // sprite location is X - 1 through X + 1
+    // drawing pixel: cycle % 40 of row Math.floor cycle / 40
+    const pixel = cycle % displayWidth
+    const displayRow = Math.floor(cycle / 40)
+
+    if (pixel >= X - 1 && pixel <= X + 1) {
+      display[displayRow][pixel] = '#'
+    }
+
+    cycle++
+
+    // end of cycle
+    if (cycle % displayWidth === displayWidth / 2) {
+      signalSum += X * cycle
+    }
+  }
+
+  // process cycles
+  for (let i = 0; i < dataIn.length; i++) {
+    if (dataIn[i] === 'noop') {
+      // noop takes one cycle to complete. It has no other effect.
+      doCycleAndCalculateSignal()
+    } else {
+      // addx V takes two cycles to complete.
+      // After two cycles, the X register is increased by the value V. (V can be negative.)
+      const [, amount] = dataIn[i].split(' ')
+      doCycleAndCalculateSignal()
+      doCycleAndCalculateSignal()
+      X += parseInt(amount)
+    }
+  }
+
+  // print display
+  for (let i = 0; i < display.length; i++) {
+    console.log(display[i].join(''))
+  }
+
+  return { answerQuestion1: signalSum, answerQuestion2: 0 }
 }
